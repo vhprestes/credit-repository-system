d31ed391ae07e338a112f275ca93f4c2c1bdbf1f
vhprestes <vhprestes@gmail.com>
2023-05-18 18:58:39 -0300

    feat: credit dto

---
+++ src/main/kotlin/me/dio/creditapplicationsystem/dto/CreditDto.kt
@@ -0,0 +1,20 @@
+package me.dio.creditapplicationsystem.dto
+
+import me.dio.creditapplicationsystem.entity.Credit
+import me.dio.creditapplicationsystem.entity.Customer
+import java.math.BigDecimal
+import java.time.LocalDate
+
+data class CreditDto(
+    val creditValue: BigDecimal,
+    val dayFirstOfInstallment: LocalDate,
+    val numberOfInstallments: Int,
+    val customerId: Long
+) {
+    fun toEntity(): Credit = Credit(
+        creditValue = this.creditValue,
+        dayFirstInstallment = this.dayFirstOfInstallment,
+        numberOfInstallments = this.numberOfInstallments,
+        customer = Customer(id = this.customerId)
+    )
+}
